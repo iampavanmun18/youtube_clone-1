4030722cd6fa5b53a37ba3390bab480845f85bfe
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-07-03 16:25:08 +0530

    init commit for youtube clone

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube138.p.rapidapi.com"
+const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-  params: {hl: 'en', gl: 'US'},
+  params: { hl: "en", gl: "US" },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_APIKEY,
-    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key":
+      process.env.REACT_APP_YOUTUBE_API_KEY ||
+      "f40bb900a8msh2675005f65b1b85p107114jsn879dac4fc413",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
 };
 
-export const fetchDataFromApi = async(url) => {
-    const {data} = await axios.get(`${BASE_URL}/${url}`, options)
-    return data;
-}
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
